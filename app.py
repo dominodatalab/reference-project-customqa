3c70104d29e7fb51b0bc721cc44f2486fc84f502
ddl-subir-m <117772994+ddl-subir-m@users.noreply.github.com>
2023-09-20 12:38:36 -0700

    Update app.py

--- app.py
+++ app.py
@@ -121,7 +121,7 @@ with container:
     with st.form(key='my_form', clear_on_submit=True):
         user_input = st.text_area("You:", key='input', height=100)
         submit_button = st.form_submit_button(label='Send')
-    if submit_button and user_input and qa:
+    if submit_button and user_input and qa and openai_key:
         with st.spinner("Searching for the answer..."):
             with get_openai_callback() as cb:
                 result = qa({"question": user_input})
