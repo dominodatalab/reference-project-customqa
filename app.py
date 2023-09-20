206fc62dd9c7cc4a1ee422018b9a6c4c517bbb21
ddl-subir-m <117772994+ddl-subir-m@users.noreply.github.com>
2023-09-20 12:33:30 -0700

    Update app.py

--- app.py
+++ app.py
@@ -139,5 +139,5 @@ with container:
                 # message(st.session_state["generated"][i], key=str(i))
                 message(st.session_state["past"][i], is_user=True, logo='https://freesvg.org/img/1367934593.png', key=str(i) + '_user')
                 message(st.session_state["generated"][i], logo='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQk6e8aarUy37BOHMTSk-TUcs4AyAy3pfAHL-F2K49KHNEbI0QUlqWJFEqXYQvlBdYMMJA&usqp=CAU', key=str(i))
-                if 'total_tokens' in st.session_state and len(st.session_state['total_tokens']) > 0:
+                # if 'total_tokens' in st.session_state and len(st.session_state['total_tokens']) > 0:
                     # st.write(f"Number of tokens: {st.session_state['total_tokens'][i]}")
