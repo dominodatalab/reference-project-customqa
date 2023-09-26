bd64f24555076706529b3718f9e4a1ad08aaa877
Subir Mansukhani <subir.mansukhani@dominodatalab.com>
2023-09-26 15:29:02 -0700

    Pushed from Domino: https://demo2.dominodatalab.com/workspace/subir_mansukhani/RAG?executionId=6513273704b19a0efce62cc4

--- app.py
+++ app.py
@@ -39,13 +39,13 @@ def get_chain(vectorstore, model, openai_api_key):
 
     memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
     if llm:
-        qa_chain = ConversationalRetrievalChain.from_llm(llm, vectorstore.as_retriever(), memory=memory, qa_prompt=QA_PROMPT, condense_question_prompt=CONDENSE_QUESTION_PROMPT)
+        qa_chain = ConversationalRetrievalChain.from_llm(llm, vectorstore.as_retriever(), memory=memory, condense_question_prompt=CONDENSE_QUESTION_PROMPT)
 
     return qa_chain
 
 
 
-_template = """Given the following conversation and a follow up question, rephrase the follow up question to be a standalone question.
+_template = """Given the following conversation and a follow up question, rephrase the follow up question to be a standalone question. If the question is not about healthcare or insurance or do not know the answer please respond politely that you don't know the answer, do not hallucinate an answer
 
 Chat History:
 {chat_history}
@@ -54,17 +54,7 @@ Standalone question:"""
 
 CONDENSE_QUESTION_PROMPT = PromptTemplate.from_template(_template)
 
-template = """You are a helpful AI assistant for answering questions about information in Domino Data Labs product documentation.
-You are given the following extracted parts of a long document and a question. Provide a conversational answer.
-If you don't know the answer, just say "Hmm, I'm not sure." Don't try to make up an answer.
-If the question is not about AI or ML or data science or MLOps or related to Domino Data Lab, politely inform them that you are tuned to only answer questions about MLOps, data science and Domino Data Lab.
-Question: {question}
-=========
-{context}
-=========
-Answer in Markdown:"""
 
-QA_PROMPT = PromptTemplate(template=template, input_variables=["question", "context"])
 
 # Uncomment if you want to store and use the OpenAI key stored in an environment variable
 openai_key = os.getenv('OPENAI_API_KEY') 
