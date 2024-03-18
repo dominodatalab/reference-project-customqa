9aa66f8d420036b0c6870813bc46d355979f9ce9
ddl-subir-m <117772994+ddl-subir-m@users.noreply.github.com>
2024-03-18 11:31:38 -0700

    Update app.py

--- app.py
+++ app.py
@@ -110,7 +110,9 @@ with container:
     with st.form(key='my_form', clear_on_submit=True):
         user_input = st.text_area("You:", key='input', height=100)
         submit_button = st.form_submit_button(label='Send')
-    if submit_button and user_input and qa and openai_key:
+    if submit_button and not openai_key:
+        st.error("OpenAI API key is missing.Please provide it in the sidebar or set it in an environment variable")
+    elif submit_button and user_input and qa and openai_key:
         with st.spinner("Searching for the answer..."):
             with get_openai_callback() as cb:
                 result = qa({"question": user_input})
